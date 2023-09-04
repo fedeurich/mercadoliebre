163c60132d20c3f4b2a3bd70d4a342c17488844b
Jorge Federico Urich <federicourichlc@gmail.com>
2023-09-03 22:53:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express= require("express");
+
+const app=express();
+
+const path=require("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{ console.log("El Servidor esta correindo en el puerto http://localhost:3000/");});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+  });
+  app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+  })
+
+  app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+  })
