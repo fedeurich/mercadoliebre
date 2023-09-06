1840d272b0b92e7900390834067ff5ea198ac16f
Jorge Federico Urich <federicourichlc@gmail.com>
2023-09-06 00:26:02 -0300

    Buscando renderizar

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const PORT= process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(PORT,()=>{ console.log(`El Servidor esta correindo en el puerto ${PORT}`);});
+app.listen(PORT,()=>{ console.log(`El Servidor esta correindo en el puerto http://localhost:${PORT}`);});
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"));
   });
-  app.get("/", (req,res)=>{
+  app.get("/register.html", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
   })
 
-  app.get("/", (req,res)=>{
+  app.get("/login.html", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
   })
